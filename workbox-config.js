ea21e16a0c12ad9a7af14a1b321307355d065024
Tholm43 <104298171+Tholm43@users.noreply.github.com>
2023-06-28 13:40:15 -0500

    Delete workbox-config.js

--- workbox-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    globDirectory: 'dist/',
-    globPatterns: ['**/*.{png,html,css,js}'],
-    swDest: 'dist/sw.js',
-    ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
-    cleanupOutdatedCaches: true,
-	sourcemap: false,
-};
