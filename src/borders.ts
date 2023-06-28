38d186022ecf30e1a5bc0a416e765bf48f5e382a
Tholm43 <104298171+Tholm43@users.noreply.github.com>
2023-06-28 13:39:41 -0500

    Delete borders.ts

--- src/borders.ts
+++
@@ -1,7 +0,0 @@
-import * as fabric from 'fabric';
-
-const canvasContainer = document.querySelector('.canvas-container');
-const w = canvasContainer?.clientWidth || 1000;
-const h = canvasContainer?.clientHeight || 1000;
-
-const canvasElement = document.getElementById('canvas') as HTMLCanvasElement;
